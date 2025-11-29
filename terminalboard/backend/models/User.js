f7be12cd88c4ef41660c919ea91c872a90683816
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2025-11-29 22:26:25 +0400

    Create User.js

---
+++ terminalboard/backend/models/User.js
@@ -0,0 +1,34 @@
+// backend/models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
